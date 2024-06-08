213070c5ceeeb13e1b788dd03b1917734de87a58
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-06-08 02:13:19 -0400

    Update Browse.js

--- src/Browse.js
+++ src/Browse.js
@@ -5,53 +5,25 @@ import { listItems } from './product-data'
 
 
 const Browse=()=>{
-  let Data=listItems.map((data)=>{
-    return(
-       <Product
-      key={data.id}
-      title={data.foodName}
-      rating={data.rating}
-      weight={data.weight}
-      description={data.description}
-      cost={data.cost}
-      img={data.image}/>)
-  })
+
+  let Data=listItems;
+  // let Data=listItems.map((data)=>{
+  //   return(
+  //      <Product
+  //     key={data.info.id}
+  //     title={data.info.name}
+  //     rating={data.info.avgRating}
+  //     weight={data.info.costForTwo}
+  //     description={data.info.areaName}
+  //     cost={data.info.price}
+  //     img={data.info.cloudinaryImageId}/>)
+  // })
 
   return(
     <body>
-      {Data}
+      {Data.map((res) => (<Product r={res}/>))}
     </body>
   )
 }
 
 export default Browse
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let Data=listItems
-// const Browse = () => {
-//  console.log(Data)
-  
-//   return (
-//     <body>
-//       {Data.map((res)=>{<Product Data1={(console.log(Data))}
-//       />})}
-//     </body>
-//   )
-// }
-
-// export default Browse
